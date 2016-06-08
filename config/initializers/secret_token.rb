228ef82360f9065458c10d6ae3b6617b8890ef99
Kevin Hathorn <hathornk@hotmail.com>
2016-06-08 13:07:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DigIn::Application.config.secret_key_base = '705c8cc510b753046807f38350a30b8d23f95ed74b35a649f6523995847c4a5ce95b060b6805f958f95579cc2a7fd0bf0028dc5954e9714734ba51c3fba2b81a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DigIn::Application.config.session_store :cookie_store, key: '_dig-in_session'
